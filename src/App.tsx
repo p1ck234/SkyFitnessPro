aa959857e2b42838a319ddc5e931e7f78ec94b41
p1ck23 <danil-vetrov47@mail.ru>
2024-07-27 18:29:16 +0400

    fix dont work project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return 
+  return <div>asdf</div>;
 }
-export default App
+export default App;
