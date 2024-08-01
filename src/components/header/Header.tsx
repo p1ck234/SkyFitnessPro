fc805f8d83195f4fda167928c45f537b5167ec70
p1ck23 <dagobbo17@gmail.com>
2024-08-01 21:55:33 +0400

    functional popups

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import "../../index.css";
-import { Link } from "react-router-dom";
-import { constRoutes } from "../../lib/paths";
-const Header = () => {
+
+interface HeaderProps {
+  openModal: (isLoginMode: boolean) => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ openModal }) => {
   return (
     <>
-      <header className="flex justify-between items-center py-4 p-48">
+      <header className="flex justify-between items-center py-4 px-48">
         <div className="flex items-center">
           <img src="logo.svg" alt="Logo" className="h-8 mr-2" />
         </div>
-        <Link to={constRoutes.LOGIN}>
-        <button className="bg-customGreen text-black py-2 px-4 rounded-lg">
+        <button
+          onClick={() => openModal(true)}
+          className="bg-customGreen text-black py-2 px-4 rounded-lg"
+        >
           Войти
         </button>
-        </Link>
       </header>
     </>
   );
