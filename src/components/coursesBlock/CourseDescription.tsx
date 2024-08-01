1ab110293cbed19a5ea5fb3f9bcdf91ef01571ab
p1ck23 <dagobbo17@gmail.com>
2024-08-01 22:30:14 +0400

    setting course page

--- src/components/coursesBlock/CourseDescription.tsx
+++ src/components/coursesBlock/CourseDescription.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+
 const CourseDescription = () => {
   return (
-    <>
-     {/* Benefits Section */}
-     <section className="mb-6 mt-14">
+    <div className="flex flex-col gap-16">
+      {/* Benefits Section */}
+      <section className="mb-6 mt-14">
         <h2 className="text-5xl font-bold mb-4">Подойдет для вас, если:</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-10">
           <div className="bg-gray-800 text-white p-6 rounded-lg flex items-center">
@@ -67,28 +68,15 @@ const CourseDescription = () => {
 
       {/* Course Description Section */}
       <section
-        className="relative flex bg-white rounded-lg shadow-lg p-6 mb-6"
-        style={{
-          minHeight: "486px",
-          borderRadius: "30px",
-          boxShadow: "0px 4px 67px -12px rgba(0, 0, 0, 0.13)",
-          marginTop: "104px",
-          zIndex: "1",
-        }}
+        className="flex bg-white rounded-lg shadow-custom p-6 mb-6 rounded-3xl shadow-boxShadow"
+        
       >
-        <div className="flex flex-col justify-between h-full pr-6 gap-8">
-          <h2 className="text-6xl font-medium mb-4 font-bold ">
+        <div className="flex flex-col justify-between h-full pr-6 gap-8 w-1/2">
+          <h2 className="text-5xl font-medium mb-4 font-bold">
             Начните путь
             <br /> к новому телу
           </h2>
-          <ul
-            className="list-disc pl-5 mb-4 text-xl text-gray-700"
-            style={{
-              opacity: "0.6",
-              fontFamily: "Roboto",
-              lineHeight: "110%",
-            }}
-          >
+          <ul className="list-disc pl-5 opacity-60 mb-4 text-xl text-gray-700">
             <li>проработка всех групп мышц</li>
             <li>тренировка суставов</li>
             <li>улучшение циркуляции крови</li>
@@ -96,55 +84,21 @@ const CourseDescription = () => {
             <li>помогают противостоять стрессам</li>
           </ul>
 
-          <div className="flex justify-center">
+          <div className="flex">
             <button className="bg-customGreen text-black py-2 px-4 rounded-lg">
               Войдите, чтобы добавить курс
             </button>
           </div>
         </div>
-        <div className="flex-1">
+        <div className="relative w-1/2">
           <img
-            src="man_image.png"
+            src="img/runner.svg"
             alt="Course Description"
-            className="absolute"
-            style={{
-              width: "519.47px",
-              height: "539.54px",
-              left: "750.99px",
-              bottom: "45px",
-              transform: "rotate(-2.99deg)",
-              zIndex: "2",
-            }}
-          />
-          <img
-            src="long_line.svg"
-            alt="Long Line"
-            className="absolute"
-            style={{
-              width: "670.18px",
-              height: "390.98px",
-              left: "585.06px",
-              bottom: "15px",
-              transform: "rotate(-12.38deg)",
-              zIndex: "-1",
-              maskImage:
-                "linear-gradient(to bottom, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 100%)",
-            }}
-          />
-          <img
-            src="short_line.svg"
-            alt="Short Line"
-            className="absolute"
-            style={{
-              width: "50px",
-              height: "42.5px",
-              left: "872px",
-              bottom: "445px",
-            }}
+            className= "w-9/12"
           />
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
