ec8aecf66584297acf15654895bf3e830fcb4f73
p1ck234 <danil-vetrov47@mail.ru>
2024-08-08 03:35:29 +0400

    setting firebase

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD9Dmb_NQ2UnUrP5xJrdT5fAhIojhVdmm0",
   authDomain: "skyprofitness-38202.firebaseapp.com",
@@ -19,3 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { firebaseConfig }; // Добавляем экспорт
