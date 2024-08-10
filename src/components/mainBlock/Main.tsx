c9fc3df206b3e6cedbdffb417d57b7a2f4c14361
p1ck23 <dagobbo17@gmail.com>
2024-08-11 03:36:26 +0400

    setting btn up

--- src/components/mainBlock/Main.tsx
+++ src/components/mainBlock/Main.tsx
@@ -1,5 +1,6 @@
 import { Heading } from "./Heading/Heading";
 import { Cards } from "./Cards/Cards";
+import { Button } from "../Button";
 
 export function Main() {
   return (
@@ -8,9 +9,12 @@ export function Main() {
         <Heading />
         <Cards />
         <div className="flex  justify-center">
-          <button className=" font-normal text-lg grid rounded-3xl bg-customGreen px-4 py-3">
+          <a
+            href="#heading-section"
+            className=" font-normal text-lg grid rounded-3xl bg-customGreen px-4 py-3"
+          >
             Наверх ↑
-          </button>
+          </a>
         </div>
       </div>
     </>
