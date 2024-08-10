0b6983515cb84e8ae2fd56bf43fb849e09735d34
p1ck23 <dagobbo17@gmail.com>
2024-08-11 02:54:53 +0400

    adaptive mainpage

--- src/components/mainBlock/Heading/Heading.tsx
+++ src/components/mainBlock/Heading/Heading.tsx
@@ -1,10 +1,10 @@
 export function Heading() {
   return (
     <div className="flex gap-5 mt-14">
-      <h2 className="flex-initial w-3/4 font-medium text-6xl">
+      <h2 className="flex-initial w-3/4 font-medium text-2xl phone:text-6xl">
         Начните заниматься спортом и улучшите качество жизни
       </h2>
-      <div className="bg-customGreen text-center items-center flex ">
+      <div className="bg-customGreen text-center items-center flex hidden phone:flex">
         <p className="text-3xl">Измени своё тело за полгода!</p>
       </div>
     </div>
