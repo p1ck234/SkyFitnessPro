44e052268b8c70165b09f327b2e65d93a2fa6eab
p1ck23 <dagobbo17@gmail.com>
2024-08-11 07:06:47 +0400

    course functional BD

--- src/components/coursesBlock/CoursesBlock.tsx
+++ src/components/coursesBlock/CoursesBlock.tsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImageComponent } from "../imageComponent/ImageComponent";
+import { Course } from "@/types/types";
 
-const CoursesBlock = () => {
+interface CoursesBlockProps {
+  course: Course; 
+}
+
+const CoursesBlock: React.FC<CoursesBlockProps> = ({ course }) => {
+  const [isImageLoading, setIsImageLoading] = useState(true); // Состояние загрузки изображения
+
   return (
     <div>
-      {/* Hero Section */}
       <section
-        className="relative bg-customYellow rounded-lg p-8 mb-6 items-center overflow-hidden"
+        className="relative p-8 mb-6 items-center"
         style={{ height: "300px" }}
       >
-        <h1 className="absolute text-3xl md:text-6xl font-bold text-white z-10 hidden phone:block">
-          Йога
+        <h1 className="absolute text-3xl md:text-6xl font-bold text-white z-10 hidden phone:block p-6">
+          {course.name}
         </h1>
-        <img
-          src="yoga.png"
-          alt="Hero"
-          className="absolute inset-y-100 right-1 bottom-12"
+
+        {isImageLoading && (
+          <div className="absolute inset-0 flex justify-center items-center">
+            <div className="loader" /> {/* Лоадер, можно использовать любой компонент для отображения загрузки */}
+          </div>
+        )}
+
+        <ImageComponent
+          filePath={course.img}
+          className="w-full h-auto rounded-lg object-cover"
+          onLoad={() => setIsImageLoading(false)} // Убираем лоадер после загрузки изображения
+          onError={() => setIsImageLoading(false)} // Убираем лоадер если произошла ошибка при загрузке
         />
       </section>
     </div>
@@ -22,27 +37,3 @@ const CoursesBlock = () => {
 };
 
 export default CoursesBlock;
-
-// import React from "react";
-
-// const CoursesBlock = () => {
-//   return (
-//     <div>
-//       <section
-//         className="relative bg-customYellow rounded-lg p-4 md:p-8 items-center overflow-hidden flex flex-col md:flex-row justify-between"
-//         style={{ height: "300px" }} // Возвращаем высоту на десктопах
-//       >
-//         <h1 className="absolute text-3xl md:text-6xl font-bold text-white z-10 hidden phone:block">
-//           Йога
-//         </h1>
-//         <img
-//           src="/img/themeBig_Yoga.svg"
-//           alt="Hero"
-//           className="left-2" // Сдвигаем изображение влево на мобильных устройствах
-//         />
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default CoursesBlock;
