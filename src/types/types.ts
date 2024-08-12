a572ecbaa9b27fcbd142b834805c700c835320a9
p1ck23 <dagobbo17@gmail.com>
2024-08-13 02:37:16 +0400

    edit types

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,7 +5,7 @@ export type Course = {
   imgMobile: string;
   days: number;
   range: string;
-  good_for_you: string[];    // Новый массив строк
-  directions: string[];      // Новый массив строк
-  description: string[];     // Новый массив строк
+  good_for_you: string[];   
+  directions: string[];      
+  description: string[];    
 };
