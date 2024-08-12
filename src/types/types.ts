72f55c4e15e035409e54025c538b96d497d9ec8e
p1ck23 <dagobbo17@gmail.com>
2024-08-12 21:00:41 +0400

    ubdate bd

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,4 +5,7 @@ export type Course = {
   imgMobile: string;
   days: number;
   range: string;
+  good_for_you: string[];    // Новый массив строк
+  directions: string[];      // Новый массив строк
+  description: string[];     // Новый массив строк
 };
