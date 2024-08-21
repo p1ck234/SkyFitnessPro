5b36bcf5b4557956bdf4ed5212c825b25487cf79
Inna-Ser <serebrakovainna1@gmail.com>
2024-08-20 21:35:41 -0500

    conseled

--- src/components/coursesBlock/Course.tsx
+++ src/components/coursesBlock/Course.tsx
@@ -3,12 +3,11 @@ import { useParams } from "react-router-dom";
 import CoursesBlock from "./CoursesBlock";
 import CourseDescription from "./CourseDescription";
 import { Button } from "../shared/Button";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
+import { useCourses } from "@/context/courseContext";
 
 const Course: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const { courses, loading } = useSelector((state:RootState) => state.course);
+  const { courses, loading } = useCourses();
 
   if (loading) {
     return (
